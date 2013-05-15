6160939226ad1f9035af3a5d2b9bc75026c0a02a
schasse <sebastian.schasse@gapfish.com>
2013-05-15 15:39:50 +0200

    waste just 256mb

--- lib/waste-memory.rb
+++ lib/waste-memory.rb
@@ -1,21 +1,8 @@
 module WasteMemory
 
-  def self.waste_memory(size = 256)
-    # get binary representation of size
-    binary_size = size.to_s(2).reverse.split('')
-
-    # waste memory
-    binary_size.each do |digit|
-      if digit == '1'
-        waste_two_power binary_size.index(digit)
-      end
-    end
-    nil
-  end
-
-  def self.waste_two_power(pow = 0)
-    # create string, which is one mb of size
+  def self.waste_256_mb_memory
+    # create 256mb big string
     mb = '1'
-    (19 + pow).times { mb += mb }
+    28.times { mb += mb }
   end
 end
