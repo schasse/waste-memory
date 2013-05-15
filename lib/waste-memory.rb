f1bac91db8c0cf3b96fa18e43b53485321ec8c41
schasse <sebastian.schasse@gapfish.com>
2013-05-15 15:29:35 +0200

    do it another way

--- lib/waste-memory.rb
+++ lib/waste-memory.rb
@@ -1,22 +1,21 @@
 module WasteMemory
 
   def self.waste_memory(size = 256)
-    # create string, which is one mb of size
-    mb = '1'
-    21.times { mb += mb }
-
     # get binary representation of size
     binary_size = size.to_s(2).reverse.split('')
 
     # waste memory
-    big_thing = []
     binary_size.each do |digit|
       if digit == '1'
-        thing = mb
-        binary_size.index(digit).times { thing += thing }
-        big_thing << thing
+        waste_two_power binary_size.index(digit)
       end
     end
     nil
+  end
+
+  def self.waste_two_power(pow = 0)
+    # create string, which is one mb of size
+    mb = '1'
+    (19 + pow).times { mb += mb }
   end
 end
